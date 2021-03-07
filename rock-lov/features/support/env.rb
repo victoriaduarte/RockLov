e58a6c9be2b1c4262aa4c30a2e52a9709ad53226
victoria.duarte <victoria.duarte@netprecision.com.br>
2021-03-07 15:09:57 -0300

    Sets up Allure Report

--- rock-lov/features/support/env.rb
+++ rock-lov/features/support/env.rb
@@ -1,3 +1,4 @@
+require "allure-cucumber"
 require "capybara"
 require "capybara/cucumber"  # importa o capybara para o projeto cucumber
 
@@ -5,4 +6,9 @@ Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   config.app_host = "http://rocklov-web:3000"
   config.default_max_wait_time = 15
+end
+
+AllureCucumber.configure do |config|
+  config.results_directory = "/logs"
+  config.clean_results_directory = true
 end
