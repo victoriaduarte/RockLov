aeed2ee22fb8456e5147d039b2bb5c829c79b569
victoria.duarte <victoria.duarte@netprecision.com.br>
2021-03-07 20:32:44 -0300

    Update browser profiles

--- rock-lov/features/support/env.rb
+++ rock-lov/features/support/env.rb
@@ -4,8 +4,21 @@ require "capybara/cucumber"  # importa o capybara para o projeto cucumber
 
 CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["CONFIG"]}"))
 
+case ENV["BROWSER"]
+when "firefox"
+  @driver = :selenium
+when "chrome"
+  @driver = :selenium_chrome
+when "fire_headless"
+  @driver = :selenium_headless
+when "chrome_headless"
+  @driver = :selenium_chrome_headless
+else
+  raise "Navegador incorreto, variável @driver está vazia :("
+end
+
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  config.default_driver = @driver
   config.app_host = CONFIG["url"]
   config.default_max_wait_time = 15
 end
