26686dfd0a42e735c183d70184b5f5c6030127a6
victoria.duarte <victoria.duarte@netprecision.com.br>
2021-03-07 16:29:14 -0300

    Sets up test environments and Allure screenshots

--- rock-lov/features/support/env.rb
+++ rock-lov/features/support/env.rb
@@ -2,9 +2,11 @@ require "allure-cucumber"
 require "capybara"
 require "capybara/cucumber"  # importa o capybara para o projeto cucumber
 
+CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["CONFIG"]}"))
+
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
-  config.app_host = "http://rocklov-web:3000"
+  config.app_host = CONFIG["url"]
   config.default_max_wait_time = 15
 end
 
--- rock-lov/features/support/hooks.rb
+++ rock-lov/features/support/hooks.rb
@@ -8,3 +8,13 @@ Before do # executa esse código antes de cada cenário
   #page.driver.browser.manage.window.maximize  # maximiza a janela
   page.current_window.resize_to(1440, 900) # tamanho padrão da janela (valor mínimo)
 end
+
+After do
+  temp_shot = page.save_screenshot("logs/temp_screenshot.png")
+
+  Allure.add_attachment(
+    name: "Screenshot",
+    type: Allure::ContentType::PNG,
+    source: File.open(temp_shot),
+  )
+end
