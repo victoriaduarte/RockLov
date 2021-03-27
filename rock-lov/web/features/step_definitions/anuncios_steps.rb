4eef947ca68146f7b4dce49488e9953f2e3d6e37
victoria.duarte <victoria.duarte@netprecision.com.br>
2021-03-27 18:50:43 -0300

    Add announcement removal

--- rock-lov/web/features/step_definitions/anuncios_steps.rb
+++ rock-lov/web/features/step_definitions/anuncios_steps.rb
@@ -3,6 +3,9 @@ Dado("Login com {string} e {string}") do |email, password|
 
   @login_page.open
   @login_page.login(email, password)
+
+  # checkpoint para garantir que estamos no dashboard
+  expect(@dash_page.on_dash?).to be true
 end
 
 Dado("que acesso o formulario de cadastro de anuncios") do
@@ -27,4 +30,46 @@ end
 
 Então("deve conter a mensagem de alerta: {string}") do |expect_alert|
   expect(@alert.dark).to have_text expect_alert
+end
+
+# Remover anúncios
+
+Dado("que eu tenho um anúncio indesejado:") do |table|
+  user_id = page.execute_script("return localStorage.getItem('user')")
+
+  thumbnail = File.open(File.join(Dir.pwd, "features/support/fixtures/images", table.rows_hash[:thumb]), "rb")
+
+  @equipo = {
+    thumbnail: thumbnail,
+    name: table.rows_hash[:nome],
+    category: table.rows_hash[:categoria],
+    price: table.rows_hash[:preco],
+  }
+
+  EquiposService.new.create(@equipo, user_id)
+
+  visit current_path
+end
+
+Quando("eu solicito a exclusão desse item") do
+  @dash_page.request_removal(@equipo[:name])
+  sleep 1 # think time
+end
+
+Quando("confirmo a exclusão") do
+  @dash_page.confirm_removal
+end
+
+Quando("não confirmo a solicitação") do
+  @dash_page.cancel_removal
+end
+
+Então("não devo ver esse item no meu dashboard") do
+  expect(
+    @dash_page.has_no_equipo?(@equipo[:name])
+  ).to be true
+end
+
+Então("esse item deve permanecer no meu Dashboard") do
+  expect(@dash_page.equipo_list).to have_content @equipo[:name]
 end
--- rock-lov/web/features/support/views/pages/dash_page.rb
+++ rock-lov/web/features/support/views/pages/dash_page.rb
@@ -12,4 +12,21 @@ class DashPage
   def equipo_list
     return find(".equipo-list")
   end
+
+  def has_no_equipo?(name)
+    return page.has_no_css?(".equipo-list li", text: name)
+  end
+
+  def request_removal(name)
+    equipo = find(".equipo-list li", text: name)
+    equipo.find(".delete-icon").click
+  end
+
+  def confirm_removal
+    click_on "Sim"
+  end
+
+  def cancel_removal
+    click_on "Não"
+  end
 end
