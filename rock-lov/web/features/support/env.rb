39e6ec1a708820a7c40d4da9b541c54d1c4eea35
victoria.duarte <victoria.duarte@netprecision.com.br>
2021-04-05 20:44:43 -0300

    Fix chrome headless mode

--- rock-lov/web/features/support/env.rb
+++ rock-lov/web/features/support/env.rb
@@ -13,16 +13,15 @@ when "fire_headless"
   @driver = :selenium_headless
 when "chrome_headless"
   Capybara.register_driver :selenium_chrome_headless do |app|
-    version = Capybara::Selenium::Driver.load_selenium
-    options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
+    Capybara::Selenium::Driver.load_selenium
     browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
-      opts.add_argument("--headless")
-      opts.add_argument("--disable-gpu")
-      opts.add_argument("--disable-site-isolation-trials")
-      opts.add_argument("--no-sandbox")
-      opts.add_argument("--disable-dev-shm-usage")
+      opts.args << "--headless"
+      opts.args << "--disable-gpu"
+      opts.args << "--disable-site-isolation-trials"
+      opts.args << "--no-sandbox"
+      opts.args << "--disable-dev-shm-usage"
     end
-    Capybara::Selenium::Driver.new(app, **{ :browser => :chrome, options_key => browser_options })
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
   end
 
   @driver = :selenium_chrome_headless
